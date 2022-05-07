fc36fa8d4232b06020fb8bb27cb013d8eafb8385
Leo Lee <tl.leolee@gmail.com>
2022-05-06 22:14:55 -0700

    add one more az

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -11,6 +11,11 @@ locals {
       public_cidr_block  = "10.0.1.0/24"
       private_cidr_block = "10.0.4.0/24"
     },
+    {
+      availability_zone  = "us-east-1c"
+      public_cidr_block  = "10.0.2.0/24"
+      private_cidr_block = "10.0.5.0/24"
+    },
   ]
 }
 
